a81eef800ab6be1e657978ac13a486391c815d97
timtitler-avalara <timtitler-avalara@users.noreply.github.com>
2015-11-18 11:56:26 -0600

    Delete RefreshReferences.bat

--- SourceCode/PicturePlinko/References/RefreshReferences.bat
+++
@@ -1,8 +0,0 @@
-copy ..\..\..\Common\Output\ZyTax.*.dll .
-copy ..\..\..\Common\Output\ZyTax.*.pdb .
-copy ..\..\..\Common\Output\ZyTax.*.xml .
-copy ..\..\..\Common\Output\pt-Br\ZyTax.*.dll .\pt-Br
-copy ..\..\..\Common\Output\zh-CN\ZyTax.*.dll .\zh-CN
-
-pause
-
